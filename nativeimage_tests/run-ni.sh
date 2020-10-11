02e9ca48bd807fe50c51c9d6d49ffc803b9b2d8c
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2020-10-11 22:27:15 +0200

    Minor script update.

--- nativeimage_tests/run-ni.sh
+++ nativeimage_tests/run-ni.sh
@@ -2,7 +2,8 @@
 
 JAVA_HOME=$HOME/git-oracle/graal/sdk/latest_graalvm_home
 
-patched=/tmp/org.graalvm.datastructure.specialization/patched/java.base
+workdir=/tmp/org.graalvm.datastructure.specialization
+patched=$workdir/patched/java.base
 
 $JAVA_HOME/bin/javac --patch-module java.base=$patched SpecializedArrayList.java
 $JAVA_HOME/bin/native-image -J--patch-module -Jjava.base=$patched SpecializedArrayList
