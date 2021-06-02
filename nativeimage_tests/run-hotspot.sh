667862fb08604a5432fe416b16f7ad3e03f38453
Rodrigo Bruno <rodrigo.b.bruno@oracle.com>
2021-06-02 12:01:28 +0100

    Added an arrayread experiment.

--- nativeimage_tests/run-hotspot.sh
+++ nativeimage_tests/run-hotspot.sh
@@ -15,16 +15,32 @@ $JAVA_HOME/bin/javac -cp $classpath --add-reads java.base=ALL-UNNAMED --patch-mo
 # Running TestingSpecializedDataStructures
 #$JAVA_HOME/bin/java $specopts -cp $classpath my.testpackage.TestingSpecializedDataStructures
 
-# Running PopSub (vanilla)
-$JAVA_HOME/bin/java $jvmopts -Xloggc:popsub-vanilla.jvm -cp bin:$specializationjar my.testpackage.PopSubTest &> popsub-vanilla.log
+function pubsub {
+	# Running PopSub (vanilla)
+	$JAVA_HOME/bin/java $jvmopts -Xloggc:popsub-vanilla.jvm -cp bin:$specializationjar my.testpackage.PopSubTest &> popsub-vanilla.log
 
-# Running PopSub (specialized)
-$JAVA_HOME/bin/java $jvmopts -Xloggc:popsub-specialized.jvm $specopts -cp $classpath my.testpackage.PopSubTest &> popsub-specialized.log
+	# Running PopSub (specialized)
+	$JAVA_HOME/bin/java $jvmopts -Xloggc:popsub-specialized.jvm $specopts -cp $classpath my.testpackage.PopSubTest &> popsub-specialized.log
 
-# Running PopSub (specialized-user)
-$JAVA_HOME/bin/java $jvmopts -Xloggc:popsub-user-specialized.jvm  $specopts -cp $classpath my.testpackage.PopSubTestSpecialized &> popsub-user-specialized.log
+	# Running PopSub (specialized-user)
+	$JAVA_HOME/bin/java $jvmopts -Xloggc:popsub-user-specialized.jvm  $specopts -cp $classpath my.testpackage.PopSubTestSpecialized &> popsub-user-specialized.log
 
-# Result processing for PopSub
-cat popsub-vanilla.log          | grep "took" | awk '{print $4}' > popsub-vanilla.dat
-cat popsub-specialized.log      | grep "took" | awk '{print $4}' > popsub-specialized.dat
-cat popsub-user-specialized.log | grep "took" | awk '{print $4}' > popsub-user-specialized.dat
+	# Result processing for PopSub
+	cat popsub-vanilla.log          | grep "took" | awk '{print $4}' > popsub-vanilla.dat
+	cat popsub-specialized.log      | grep "took" | awk '{print $4}' > popsub-specialized.dat
+	cat popsub-user-specialized.log | grep "took" | awk '{print $4}' > popsub-user-specialized.dat
+}
+#pubsub
+
+function arrayops {
+	# Running ArrayRead (vanilla)
+	$JAVA_HOME/bin/java $jvmopts -Xloggc:arrayread-vanilla.jvm -cp bin:$specializationjar my.testpackage.ArrayRead &> arrayread-vanilla.log
+
+	# Running ArrayRead (specialized-user)
+	$JAVA_HOME/bin/java $jvmopts -Xloggc:arrayread-user-specialized.jvm $specopts -cp bin:$specializationjar my.testpackage.ArrayReadSpecialized &> arrayread-user-specialized.log
+
+	# Result processing for Array
+	cat arrayread-vanilla.log          | grep "took" | awk '{print $4}' > arrayread-vanilla.dat
+	cat arrayread-user-specialized.log | grep "took" | awk '{print $4}' > arrayread-user-specialized.dat
+}
+arrayops
---
+++ nativeimage_tests/src/my/testpackage/ArrayReadSpecialized.java
@@ -0,0 +1,49 @@
+package my.testpackage;
+
+import java.util.ArrayListPoint;
+import java.util.Random;
+
+
+public class ArrayReadSpecialized {
+	public static void main(String[] args) {
+	    int runCount = 100_000;
+	    int objectCount = 10_000;
+	    long[] times = new long[runCount];
+	    Random random = new Random();
+	    ArrayListPoint array = new ArrayListPoint(objectCount);
+	    int nulls = 0;
+	
+	    System.out.println(String.format("Using %s", array.getClass()));
+	
+	    // Populate the pop queue
+	    for (int i = 0; i < objectCount; ++i) {
+	        Point p = new Point(random.nextInt(), random.nextInt());
+	        array.add(p);
+	    }
+	
+        // Calling a GC to make measurements more stable
+        System.gc();
+	    
+	    // Execute the experiments
+	    for (int run = 0; run < runCount; ++run) {
+	        // Record starting time
+	        times[run] = System.nanoTime();
+	
+	        // Execute the experiment
+	        for (int i = 0; i < array.size(); ++i) {
+	        	Point p = array.get(i);
+	        	nulls = p.x >0 ? nulls + 1 : 0;
+	        }
+	
+	        // Record the finishing time
+	        times[run] = System.nanoTime() - times[run];
+	    }
+
+        // Calling a GC to make measurements more stable
+        System.gc();
+	    
+	    for (int run = 0; run < runCount; ++run) {
+	        System.out.println(String.format("Round %s took %s ns (nulls = %d)", run, times[run], nulls));
+	    }
+	}
+}
